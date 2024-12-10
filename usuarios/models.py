3124818cb6a34ae05e17453547994e9ab4284c5b
Urielminpro705 <urielmata37@hotmail.com>
2024-12-10 14:51:49 -0600

    Pequeños cambios

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Usuario(models.Model):
-    nombre = models.CharField("Nombre del usuario", max_length=50)
-    correo = models.CharField("Correo electronico", max_length=50)
-    password = models.CharField("Contraseña del usuario", max_length=10)
+    nombre = models.CharField("Nombre del usuario", max_length=50, null=False)
+    correo = models.CharField("Correo electronico", max_length=50, null=False)
+    password = models.CharField("Contraseña del usuario", max_length=10, null=False)
     carrera = models.CharField("Carrera del usuario", max_length=50, default="")
     admin = models.BooleanField("Es cuenta de adminsitrador", default=False)
     superUser = models.BooleanField("Es cuenta de Super usuario", default=False)
